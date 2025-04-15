d5b71d95855f2c1a7d6584d3219006de215c4690
KhanhDK22 <kdao2003@gmail.com>
2025-04-15 10:22:12 +0700

    upload source code fix

--- src/main/java/com/ChatGroup/repository/ChatGroupRepository.java
+++ src/main/java/com/ChatGroup/repository/ChatGroupRepository.java
@@ -14,8 +14,6 @@ public interface ChatGroupRepository extends CrudRepository<ChatGroup, Long> {
     @Query(value = "SELECT * FROM chat_group WHERE chat_group_name = :name", nativeQuery = true)
     ChatGroup findChatGroupByName(@Param("name") String name);
 
-//    @Query(value = "SELECT CASE WHEN COUNT(*) > 0 THEN true ELSE false END FROM chat_group WHERE chat_group_name = :name", nativeQuery = true)
-//    boolean existsByName(@Param("name") String name);
 }
 
 //@Param dung de lien ket tham so trong method voi bien trong cau truy van JPQL hoặc native SQL
